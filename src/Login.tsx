4c5a0879b1bec79fc2bc2b3c6250292e73c82659
Balazs Prehoda <prehoda.balazs@gmail.com>
2020-05-08 22:39:08 +0200

    Initial commit

---
+++ src/Login.tsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { proxy } from './Proxy';
+import { TextInput } from './TextInput';
+
+type MyState = { email: string, password: string, displayName: string, register: boolean };
+
+export class Login extends Component<{}, MyState> {
+    state = { email: "", password: "", displayName: "", register: false };
+
+    CheckCode(str: string) {
+        if (str == "JQSN6F") {
+            this.setState({ displayName: "Balázs" });
+            this.setState({ email: str });
+        }
+        else
+        this.setState({ email: str });
+    }
+
+    render() {
+        return (
+            <div className="login">
+                <img src="logo512.png" width="256" />
+                <TextInput type="email" placeholder="Email (someone@example.com)" value={this.state.email}
+                    onChange={e => this.CheckCode(e)} onEnter={() => this.onClick()} autofocus={true} />
+                <TextInput type="password" placeholder="Password" value={this.state.password} onChange={e => this.setState({ password: e })} onEnter={ () => this.onClick() } />
+                {this.state.register &&
+                    <input type="text" placeholder="Display Name (Agent Smith)" value={this.state.displayName}
+                        onChange={e => this.setState({ displayName: e.target.value })} />}
+                <p>{this.state.register ? "Switch back to " : "Have no account yet? Go and "}
+                    <a href="" onClick={e => {
+                        e.preventDefault();
+                        this.setState(state => ({ register: !state.register })); // pass a function instead of object
+                    }}>
+                        {this.state.register ? "Login" : "Register"}
+                    </a>
+                </p>
+                <button type="button" onClick={() => this.onClick()}>
+                    {this.state.register ? "Register" : "Login"}
+                </button>
+                <a href="https://www.google.hu/search?q=privacy">Privacy Policy</a>
+            </div>);
+    }
+    onClick() {
+        if (this.state.register)
+            proxy.sendPacket({
+                type: "register", email: this.state.email, password: this.state.password,
+                displayName: this.state.displayName, staySignedIn: false
+            });
+        else
+            proxy.sendPacket({
+                type: "login", email: this.state.email, password: this.state.password,
+                staySignedIn: false
+            });
+    }
+}
---
+++ src/Proxy.ts
@@ -0,0 +1,45 @@
+import { OutgoingPacket, InboxDto, IncomingPacket, MessageDto } from "./chat";
+import { EventProducer } from "./EventProducer";
+
+class Proxy extends EventProducer<ProxyEventMap> {
+    inbox: InboxDto | null = null;
+    private ws: WebSocket;
+    constructor() {
+        super();
+        this.ws = new WebSocket("wss://raja.aut.bme.hu/chat/");
+        this.ws.addEventListener("open", () => {
+        });
+        this.ws.addEventListener("message", e => {
+            let p = JSON.parse(e.data) as IncomingPacket;
+            switch (p.type) {
+                case "error":
+                    alert(p.message);
+                    break;
+                case "login":
+                    this.inbox = p.inbox;
+                    this.dispatch( "login" );
+                    break;
+                case "message":
+                    let cid = p.channelId;
+                    this.inbox!.conversations.find(x => x.channelId === cid)?.lastMessages.push(p.message);
+                    this.dispatch( "message", cid, p.message );
+                    break;
+                case "conversationAdded":
+                    this.inbox!.conversations.push(p.conversation);
+                    this.dispatch( "conversation", p.conversation.channelId );
+                    break;
+            }
+        });
+    }
+
+    sendPacket(packet: OutgoingPacket) {
+        this.ws.send(JSON.stringify(packet));
+    }
+}
+export var proxy = new Proxy();
+
+interface ProxyEventMap {
+    "login": () => void;
+    "message": (channelId: string, message: MessageDto) => void;
+    "conversation": (channelId: string) => void;
+}
