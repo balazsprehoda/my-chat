e8344ec9c94730fa27cb236efa41850ce8b4c3e2
Balazs Prehoda <prehoda.balazs@gmail.com>
2020-05-08 23:04:14 +0200

    Add build dir

---
+++ build/precache-manifest.5c6116b070810c41a8e8c9b6803decde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d48dfdd1d41b8d08f013d7014c80cd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b3611e375e30baa038a",
+    "url": "./static/css/main.4f4ca461.chunk.css"
+  },
+  {
+    "revision": "5ed28e986c9d3ccfaaa3",
+    "url": "./static/js/2.0f4066fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f4066fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3611e375e30baa038a",
+    "url": "./static/js/main.dd16c0c2.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
